64b42b4ee5cf9aa3fe25640a41063d0f00a2580c
shyver <elouedabdelwahed@insat.u-carthage.tn>
2025-06-12 10:22:58 +0000

    totally removed type declaration

--- src/app/conviEditor/[folder]/[projectId]/page.tsx
+++ src/app/conviEditor/[folder]/[projectId]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import ConviEditor from '@/app/components/ConviEditor';
 
-export default function Page({ params }: { params: { folder: string; projectId: string } })  {
+export default function Page({ params })  {
 
     const projectId = decodeURIComponent(params.projectId);
     const folder = decodeURIComponent(params.folder);
