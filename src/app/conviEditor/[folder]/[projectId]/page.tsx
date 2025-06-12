32a13adf342ca5e607a3ce3e6134e505b5c407f9
shyver <elouedabdelwahed@insat.u-carthage.tn>
2025-06-12 09:56:22 +0000

    removed leaving prop from page

--- src/app/conviEditor/[folder]/[projectId]/page.tsx
+++ src/app/conviEditor/[folder]/[projectId]/page.tsx
@@ -1,24 +1,21 @@
 'use client'
-import { db } from '@/app/firebase';
-import { getDoc,doc } from 'firebase/firestore';
-import dynamic from 'next/dynamic';
 import ConviEditor from '@/app/components/ConviEditor';
 // export async function generateStaticParams() {
 //   const user = GetLocalUser();
 //   const uploads = await getDoc(doc(db,`${user.uid}/videos/`));
 //   return uploads.docs.map((doc) => ({ params: { projectId: doc.id } }))
 // }
-// interface PageProps {
-//   params: {
-//     projectId: string;
-//     folder: string;
-//   };
-//   leaving?: boolean;
-// }
+type Props = {
+  params: {
+    folder: string;
+    projectId: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
 
-export default function Page({ params, leaving }: {params: {projectId: string;folder: string}, leaving?: boolean}) {
+export default function Page({ params}: Props) {
 
     const projectId = decodeURIComponent(params.projectId);
     const folder = decodeURIComponent(params.folder);
-      return (<ConviEditor projectId={projectId} folder={folder} leaving={leaving} />)
+      return (<ConviEditor projectId={projectId} folder={folder}  />)
   }
