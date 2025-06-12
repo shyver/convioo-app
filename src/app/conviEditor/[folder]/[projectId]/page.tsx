34306fd7af4bd2dcce83932defea78d175af4aad
shyver <elouedabdelwahed@insat.u-carthage.tn>
2025-06-12 10:18:13 +0000

    destructed params type, removed type declaration

--- src/app/conviEditor/[folder]/[projectId]/page.tsx
+++ src/app/conviEditor/[folder]/[projectId]/page.tsx
@@ -1,20 +1,7 @@
 'use client'
 import ConviEditor from '@/app/components/ConviEditor';
-import { NextPage } from 'next';
-// export async function generateStaticParams() {
-//   const user = GetLocalUser();
-//   const uploads = await getDoc(doc(db,`${user.uid}/videos/`));
-//   return uploads.docs.map((doc) => ({ params: { projectId: doc.id } }))
-// }
-interface PageProps {
-  params: {
-    folder: string;
-    projectId: string;
-  };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
 
-export default function Page({ params}: PageProps)  {
+export default function Page({ params }: { params: { folder: string; projectId: string } })  {
 
     const projectId = decodeURIComponent(params.projectId);
     const folder = decodeURIComponent(params.folder);
